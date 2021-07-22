1a95275d8184b01bae072e7999e82cb7c9bb907f
Flora Huang <florahuang221@gmail.com>
2021-07-22 21:20:27 +0000

    Add day 8 and day 9 code

---
+++ day09/starter/secretMessageApp/public/js/writeMessage.js
@@ -0,0 +1,21 @@
+const submitMessage = () => {
+    console.log('Submitting message');
+    const passcodeInput = document.querySelector('#passcode');
+    const messageInput = document.querySelector('#message');
+
+    const passcodeValue = passcodeInput.value;
+    const messageValue = messageInput.value;
+
+    //send to firebase
+    firebase.database().ref().push({
+        message: messageValue,
+        passcode: passcodeValue
+    });
+
+    passcodeInput.value = '';
+    messageInput.value = '';
+};
+
+//const sendMessageButton = document.querySelector('.button');
+//console.log(sendMessageButton);
+//sendMessageButton.addEventListener('click', submitMessage);
